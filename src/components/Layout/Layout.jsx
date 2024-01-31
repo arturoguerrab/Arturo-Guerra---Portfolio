de8c600258fa02806b45689c8e134dfb07d8317d
Arturo Guerra <arturoguerrab12@gmail.com>
2024-01-30 22:55:39 -0300

    bg change

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -9,7 +9,7 @@ const Layout = ({ children }) => {
 		grid:"grid grid-cols-12 grid-rows-12 gap-0 w-screen h-screen "
 	}
 	return (
-		<div className={ style.grid + "bg-cover bg-[url('https://res.cloudinary.com/dbwomkmnq/image/upload/f_auto,q_auto/v1/Portfolio/fcy2n4h6qx2xfwkyzup5')]"}>
+		<div className={ style.grid + "bg-cover bg-[url('https://res.cloudinary.com/dbwomkmnq/image/upload/f_auto,q_auto/v1/Portfolio/qgtzaznalvcvobbq5cbp')]"}>
 			<StateBarContainer />
 
 			<SideBarContainer />
