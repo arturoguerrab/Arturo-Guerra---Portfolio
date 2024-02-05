349e1681fea548d76d4121b55e41cab89c58754b
Arturo Guerra <arturoguerrab12@gmail.com>
2024-02-05 14:03:12 -0300

    side bar position

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 			<Layout>
 				<div className="grid grid-cols-7 grid-rows-5 gap-8 w-full h-full">
 					<div className="col-span-3 row-span-2 w-full h-full bg-white opacity-50">1</div>
-					<div className="col-span-3 row-span-3 col-start-1 row-start-3 w-full h-full bg-white opacity-50">2</div>
+					<div className="col-span-3 row-span-3 col-start-1 row-start-3 w-full h-full bg-white opacity-50">hola</div>
 					<div className="col-span-2 row-span-3 col-start-4 row-start-1 w-full h-full bg-white opacity-50">3</div>
 					<div className="col-span-2 row-span-3 col-start-6 row-start-1 w-full h-full bg-white opacity-50">4</div>
 					<div className="col-span-4 row-span-2 col-start-4 row-start-4 w-full h-full bg-white opacity-50">5</div>
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,19 +3,23 @@ import StateBarContainer from "../StateBarContainer/StateBarContainer";
 import SideBarContainer from "../SideBarContainer/SideBarContainer";
 import DashboardContainer from "../DashboardContainer/DashboardContainer";
 
-
 const Layout = ({ children }) => {
 	const style = {
-		grid:"grid grid-cols-12 grid-rows-12 gap-0 w-screen min-h-full sm:h-screen "
-	}
+		grid: "grid grid-cols-12 grid-rows-12 gap-0 w-screen h-screen ",
+	};
 	return (
-		<div className={ style.grid + "bg-cover bg-[url('https://res.cloudinary.com/dbwomkmnq/image/upload/f_auto,q_auto/v1/Portfolio/qgtzaznalvcvobbq5cbp')]"}>
-			<StateBarContainer />
-
-			<SideBarContainer />
-
-			<DashboardContainer>{children}</DashboardContainer>
-		</div>
+		<>
+			<div
+				className={
+					style.grid +
+					"bg-cover bg-[url('https://res.cloudinary.com/dbwomkmnq/image/upload/f_auto,q_auto/v1/Portfolio/qgtzaznalvcvobbq5cbp')]"
+				}
+			>
+				<StateBarContainer />
+				<SideBarContainer />
+				<DashboardContainer>{children}</DashboardContainer>
+			</div>
+		</>
 	);
 };
 
